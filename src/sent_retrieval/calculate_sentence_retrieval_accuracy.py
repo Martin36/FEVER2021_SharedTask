83d8830d300f90312936150f2dd6273775a4e391
Martin <martin.funkquist@gmail.com>
2021-09-06 20:39:40 +0200

    Created script for retrieving sentences using word vectors

--- src/sent_retrieval/calculate_sentence_retrieval_accuracy.py
+++ src/sent_retrieval/calculate_sentence_retrieval_accuracy.py
@@ -1,17 +1,28 @@
-import argparse
 import unicodedata
-
-from util.util_funcs import load_jsonl
+from argparse import ArgumentError, ArgumentParser
+from util.util_funcs import load_jsonl, store_json
 from util.logger import get_logger
 
 logger = get_logger()
 
 
-def calculate_score(data, related_sents, k, print_low_recall=False):
+def calculate_score(data, related_sents, print_low_recall=False):
+    """Calculates the accuracy scores for the sentence retrieval
+
+    Args:
+        data (List[dict]): The FEVEROUS dataset
+        related_sents (List[dict]): The results from the sentence retrieval
+        print_low_recall (bool, optional): If True, will print the first 10 samples that does not have any matches. Defaults to False.
+
+    Returns:
+        Tuple(float, float, List[dict]): A tuple containing the precision, recall and no match samples
+    """
     sum_precision = 0
     sum_recall = 0
     counter = 0
     claims_with_sent_evidence = 0
+    no_match_samples = []
+
     for i in range(len(data)):
         evidence_sents = data[i]["evidence"][0]["content"]
         evidence_sents = [sent for sent in evidence_sents if "_sentence_" in sent]
@@ -19,7 +30,7 @@ def calculate_score(data, related_sents, k, print_low_recall=False):
             continue
         claims_with_sent_evidence += 1
         rel_sents_obj = related_sents[i]
-        rel_sents = rel_sents_obj["top_{}_sents".format(k)]
+        rel_sents = rel_sents_obj["top_sents"]
         assert (
             rel_sents_obj["id"] == data[i]["id"]
         )  # Make sure that the arrays are in the same order
@@ -35,6 +46,14 @@ def calculate_score(data, related_sents, k, print_low_recall=False):
         sum_precision += precision
         sum_recall += recall
 
+        if nr_of_correct_sents == 0:
+            obj = {
+                "claim": data[i]["claim"],
+                "evidence_sents": evidence_sents,
+                "related_sents": rel_sents,
+            }
+            no_match_samples.append(obj)
+
         if print_low_recall and counter < 10 and recall < 30:
             print("Retrieved sentences: {}".format(rel_sents))
             print("Correct sentences: {}".format(evidence_sents))
@@ -46,11 +65,11 @@ def calculate_score(data, related_sents, k, print_low_recall=False):
     precision = sum_precision / claims_with_sent_evidence
     recall = sum_recall / claims_with_sent_evidence
 
-    return precision, recall
+    return precision, recall, no_match_samples
 
 
 def main():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description="Calculates the accuracy of the sentence retrieval"
     )
     parser.add_argument("--data_path", default=None, type=str, help="Path to the data")
@@ -61,35 +80,43 @@ def main():
         help="Path to the top k docs from the document retriever",
     )
     parser.add_argument(
-        "--k",
-        default=None,
-        type=int,
-        help="The number of retrieved sentences for each claim",
+        "--out_file", default=None, type=str, help="Path to the output file"
     )
 
     args = parser.parse_args()
 
     if not args.data_path:
-        raise RuntimeError("Invalid train data path")
+        raise ArgumentError("Invalid train data path")
     if ".jsonl" not in args.data_path:
-        raise RuntimeError(
+        raise ArgumentError(
             "The train data path should include the name of the .jsonl file"
         )
     if not args.top_k_sents_file:
-        raise RuntimeError("Invalid top k sents path")
+        raise ArgumentError("Invalid top k sents path")
     if ".jsonl" not in args.top_k_sents_file:
-        raise RuntimeError(
+        raise ArgumentError(
             "The top k docs sents file path should include the name of the .jsonl file"
         )
-    if not args.k:
-        raise RuntimeError("Invalid argument: 'k'")
 
     data = load_jsonl(args.data_path)[1:]
     related_sents = load_jsonl(args.top_k_sents_file)
 
-    precision, recall = calculate_score(data, related_sents, args.k)
+    precision, recall, no_match_samples = calculate_score(data, related_sents)
     logger.info("Precision for top k sentences: {}".format(precision))
     logger.info("Recall for top k sentences: {}".format(recall))
+
+    if args.out_file:
+        if ".json" not in args.out_file:
+            raise ArgumentError(
+                "The output file path should include the name of the .json file"
+            )
+        result = {
+            "precision": precision,
+            "recall": recall,
+            "no_match_samples": no_match_samples,
+        }
+        store_json(result, args.out_file, indent=2)
+        logger.info("Stored results in '{}'".format(args.out_file))
 
 
 if __name__ == "__main__":
--- src/sent_retrieval/sent_retrieval_word_vectors.py
+++ src/sent_retrieval/sent_retrieval_word_vectors.py
@@ -1,10 +1,19 @@
-import os, sys, argparse
-
+import os, sys, nltk
+import gensim.downloader
+import numpy as np
+from argparse import ArgumentParser, ArgumentError
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
+from sent_retrieval.calculate_sentence_retrieval_accuracy import calculate_score
 
-from util.util_funcs import load_jsonl, replace_entities, extract_sents, store_jsonl
+from util.util_funcs import (
+    load_jsonl,
+    extract_sents,
+    remove_stopwords,
+    store_jsonl,
+    unique,
+)
 from util.logger import get_logger
 
 DIR_PATH = os.path.abspath(os.getcwd())
@@ -14,33 +23,9 @@ sys.path.insert(0, FEVEROUS_PATH)
 from database.feverous_db import FeverousDB
 from utils.wiki_page import WikiPage
 
+nltk.download("punkt")
+
 logger = get_logger()
-
-
-# TODO: The table parts are currently not used
-def extract_table_text(table):
-    cell_ids = table.get_ids()
-    table_rows = []
-    for i, cell_id in enumerate(cell_ids):
-        if "table_caption" in cell_id:
-            continue
-        cell_id_list = cell_id.split("_")
-        row = int(cell_id_list[-2])
-        if len(table_rows) < row + 1:
-            table_rows.append(replace_entities(table.get_cell_content(cell_id)))
-        else:
-            table_rows[row] += " " + replace_entities(table.get_cell_content(cell_id))
-    return table_rows
-
-
-def extract_tables(doc_json):
-    page = WikiPage(doc_json["title"], doc_json)
-    tables = page.get_tables()
-    tables_content = []
-    for table in tables:
-        table_rows = extract_table_text(table)
-        tables_content.append(table_rows)
-    return tables_content
 
 
 def expand_table_id(db, table_id):
@@ -61,11 +46,10 @@ def expand_table_id(db, table_id):
     return result
 
 
-def get_top_sents(db, doc_ids, claim, use_tables, n_gram_min, n_gram_max, nr_of_sents):
+def get_top_sents(db, glove_vectors, doc_ids, claim, nr_of_sents):
     sent_ids = []
-    table_ids = []
     all_sents = []
-    all_table_rows = []
+
     for doc_id in doc_ids:
         doc_json = db.get_doc_json(doc_id)
         sents = extract_sents(doc_json)
@@ -73,37 +57,28 @@ def get_top_sents(db, doc_ids, claim, use_tables, n_gram_min, n_gram_max, nr_of_
             sent_ids.append("{}_sentence_{}".format(doc_json["title"], i))
         all_sents += sents
 
-        if use_tables:
-            tables_content = extract_tables(doc_json)
-            for i, table_content in enumerate(tables_content):
-                for j in range(len(table_content)):
-                    table_ids.append("{}_table_{}_{}".format(doc_json["title"], i, j))
-                all_table_rows += table_content
+    sent_id_to_score = {}
+    claim_tokens = remove_stopwords(claim.split())
+    for id, sent in zip(sent_ids, all_sents):
+        sent_tokens = remove_stopwords(sent.split())
+        similarity = glove_vectors.wmdistance(claim_tokens, sent_tokens)
+        sent_id_to_score[id] = similarity
 
-    sent_vectorizer = TfidfVectorizer(
-        analyzer="word", stop_words="english", ngram_range=(n_gram_min, n_gram_max)
+    sent_id_to_score = dict(
+        sorted(sent_id_to_score.items(), key=lambda i: i[1], reverse=True)
     )
-    sent_wm = sent_vectorizer.fit_transform(all_sents + all_table_rows)
-    claim_tfidf = sent_vectorizer.transform([claim])
-    cosine_similarities = cosine_similarity(claim_tfidf, sent_wm).flatten()
-    top_sents_indices = cosine_similarities.argsort()[: -nr_of_sents - 1 : -1]
-    top_sents = [
-        sent for i, sent in enumerate(sent_ids + table_ids) if i in top_sents_indices
-    ]
 
-    for sent in top_sents:
-        if "_table_" in sent:
-            top_sents += expand_table_id(db, sent)
-    top_sents = [sent for sent in top_sents if "_table_" not in sent]
-    top_sents = list(set(top_sents))
+    top_sents = list(sent_id_to_score.keys())[:nr_of_sents]
 
     return top_sents
 
 
-def get_top_sents_for_claims(
-    db_path, top_docs_file, nr_of_sents, use_tables, n_gram_min, n_gram_max
-):
+def get_top_sents_for_claims(db_path, top_docs_file, nr_of_sents):
     db = FeverousDB(db_path)
+
+    logger.info("Loading word vector model...")
+    glove_vectors = gensim.downloader.load("glove-wiki-gigaword-200")
+    logger.info("Word vector model loaded")
 
     logger.info("Loading previously retrieved docs for claims...")
     top_k_docs = load_jsonl(top_docs_file)
@@ -112,13 +87,7 @@ def get_top_sents_for_claims(
     result = []
     for obj in tqdm(top_k_docs):
         top_sents = get_top_sents(
-            db,
-            obj["docs"],
-            obj["claim"],
-            use_tables,
-            n_gram_min,
-            n_gram_max,
-            nr_of_sents,
+            db, glove_vectors, obj["docs"], obj["claim"], nr_of_sents
         )
         obj["top_sents"] = top_sents
         result.append(obj)
@@ -127,12 +96,7 @@ def get_top_sents_for_claims(
 
 
 def get_top_sents_for_claim(
-    db_path: str,
-    top_k_docs: list,
-    claim: str,
-    nr_of_sents: int,
-    n_gram_min=1,
-    n_gram_max=3,
+    db_path: str, top_k_docs: list, claim: str, nr_of_sents: int
 ):
     """ Retrieves the top sentences for a claim from the previously retrieved documents
 
@@ -144,27 +108,23 @@ def get_top_sents_for_claim(
             The previously retrieved top docs for the claim
         nr_of_sents : int
             The number of sentences to retrieve
-        n_gram_min : int
-            The smallest n-gram to use in the retrieval (default is 1 e.g. unigram)
-        n_gram_max : int
-            The largest n-gram to use in the retrieval (default is 3 e.g. trigram)
     """
 
     db = FeverousDB(db_path)
-    use_tables = False
-    top_sents = get_top_sents(
-        db, top_k_docs, claim, use_tables, n_gram_min, n_gram_max, nr_of_sents
-    )
+    top_sents = get_top_sents(db, top_k_docs, claim, nr_of_sents)
 
     return top_sents
 
 
 def main():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description="Retrieves the most similar sentences from the given documents"
     )
     parser.add_argument(
         "--db_path", default=None, type=str, help="Path to the FEVEROUS database"
+    )
+    parser.add_argument(
+        "--data_path", default=None, type=str, help="Path to the data file"
     )
     parser.add_argument(
         "--top_docs_file",
@@ -190,35 +150,23 @@ def main():
         action="store_true",
         help="Tells the script if it should use table content when matching",
     )
-    parser.add_argument(
-        "--n_gram_min",
-        default=1,
-        type=int,
-        help="The lower bound of the ngrams, e.g. 1 for unigrams and 2 for bigrams",
-    )
-    parser.add_argument(
-        "--n_gram_max",
-        default=1,
-        type=int,
-        help="The upper bound of the ngrams, e.g. 1 for unigrams and 2 for bigrams",
-    )
 
     args = parser.parse_args()
 
     if not args.db_path:
-        raise RuntimeError("Invalid database path")
+        raise ArgumentError("Invalid database path")
     if ".db" not in args.db_path:
-        raise RuntimeError("The database path should include the name of the .db file")
+        raise ArgumentError("The database path should include the name of the .db file")
     if not args.top_docs_file:
-        raise RuntimeError("Invalid top docs path")
+        raise ArgumentError("Invalid top docs path")
     if ".jsonl" not in args.top_docs_file:
-        raise RuntimeError(
+        raise ArgumentError(
             "The top docs path should include the name of the .jsonl file"
         )
     if not args.out_file:
-        raise RuntimeError("Invalid output file path")
+        raise ArgumentError("Invalid output file path")
     if ".jsonl" not in args.out_file:
-        raise RuntimeError(
+        raise ArgumentError(
             "The output file path should include the name of the .jsonl file"
         )
 
@@ -228,18 +176,24 @@ def main():
         )
     )
     top_sents = get_top_sents_for_claims(
-        args.db_path,
-        args.top_docs_file,
-        args.nr_of_sents,
-        args.use_tables,
-        args.n_gram_min,
-        args.n_gram_max,
+        args.db_path, args.top_docs_file, args.nr_of_sents
     )
     logger.info("Finished retrieving top sentences")
 
     logger.info("Storing top sentences...")
     store_jsonl(top_sents, args.out_file)
     logger.info("Top sents for each claim stored in {}".format(args.out_file))
+
+    if args.data_path:
+        if ".jsonl" not in args.data_path:
+            raise ArgumentError(
+                "The train data path should include the name of the .jsonl file"
+            )
+        data = load_jsonl(args.data_path)[1:]
+        precision, recall = calculate_score(data, top_sents)
+        logger.info("===== Results =======")
+        logger.info("Precision: {}".format(precision))
+        logger.info("Recall: {}".format(recall))
 
 
 if __name__ == "__main__":
